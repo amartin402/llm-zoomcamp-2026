754b4b12e7e2f6c002053d618e08f4f51a49a41a
amartin402 <122790691+amartin402@users.noreply.github.com>
2026-07-19 11:05:07 +0000

    Update files

--- 05-monitoring/assistant.py
+++ 05-monitoring/assistant.py
@@ -1,71 +1,16 @@
-# # =========================================================
-# # Imports
-# # =========================================================
-
-# import sys
+import sys
 
 # # from dotenv import load_dotenv
-# from openai import OpenAI
+from openai import OpenAI
 
-# from ingest import load_faq_data, build_index
+from ingest import load_faq_data, build_index
 # from rag_helper import RAGBase
+from metrics import RAGWithMetrics
+from db_save import save_conversation
 
 # # =========================================================
 # # A function to create the assistant:
 # # =========================================================
-
-# def create_assistant():
-#     # load_dotenv()
-
-#     documents = load_faq_data()
-#     index = build_index(documents)
-
-#     return RAGBase(
-#         index=index,
-#         llm_client=OpenAI(),
-#     )
-
-# # =========================================================
-
-# # =========================================================
-# if __name__ == "__main__":
-#     assistant = create_assistant()
-
-#     query = "How do I join the course?"
-#     if len(sys.argv) > 1:
-#         query = sys.argv[1]
-
-#     answer = assistant.rag(query)
-#     print(answer)
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-# # =========================================================
-
-import sys
-
-# from dotenv import load_dotenv
-from openai import OpenAI
-
-from ingest import load_faq_data, build_index
-from metrics import RAGWithMetrics
-
 def create_assistant():
     # load_dotenv()
 
@@ -74,5 +19,22 @@ def create_assistant():
 
     return RAGWithMetrics(
         index=index,
-        llm_client=OpenAI()
+        llm_client=OpenAI(),
     )
+
+# # =========================================================
+
+# # =========================================================
+if __name__ == "__main__":
+    assistant = create_assistant()
+
+    query = "How do I join the course?"
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+
+    answer = assistant.rag(query)
+    print(answer)
+
+    save_conversation(assistant.last_call, query, "llm-zoomcamp")
+
+# # =========================================================
